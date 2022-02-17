640c1869810e079365f595f7d93ae1060d7c1905
Seymay <seymanuryavuz99@gmail.com>
2022-02-17 19:39:31 +0300

    first push

---
+++ js.js
@@ -0,0 +1,28 @@
+let kullanici=prompt("lütfen adınızı giriniz");
+document.querySelector("#myName").innerHTML=kullanici;
+const haftalar=[
+    "pazartesi",
+    "Salı",
+    "Çarşamba",
+    "Perşembe",
+    "Cuma",
+    "Cumartesi",
+    "Pazar"
+]
+
+function showTime()
+{    
+   let clock= new Date();
+   let saat= clock.getHours();
+   let saniye= clock.getSeconds();
+   let dakika= clock.getMinutes();
+   let gün=haftalar[clock.getDay()];
+    let zaman= saat+ " :"+ dakika+ ":" + saniye+ " :"+ gün;
+   document.querySelector("#myClock").innerHTML=zaman;
+
+   var t = setTimeout(function () {
+    showTime();
+  }, 1000);
+}
+
+showTime();
